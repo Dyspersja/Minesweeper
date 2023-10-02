b852654f1ca62bdb18db68f430483623e783bd16
Dyspersja <dyspersjamusic@gmail.com>
2023-10-02 10:44:34 +0200

    Removed template code

---
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/MainApplication.java
@@ -0,0 +1,12 @@
+package com.dyspersja.minesweeper;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
