b9abb816d4b810fee4783e2c4f6c27aed2216bbd
Dyspersja <dyspersjamusic@gmail.com>
2023-10-05 08:41:56 +0200

    moved strings to separate variables

--- Minesweeper/src/main/java/com/dyspersja/minesweeper/MainApplication.java
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/MainApplication.java
@@ -7,11 +7,14 @@ import javafx.stage.Stage;
 
 public class MainApplication extends Application {
 
+    private final String WINDOW_TITLE = "Minesweeper";
+    private final String WELCOME_SCREEN_FXML = "/fxml/WelcomeScreen.fxml";
+
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/WelcomeScreen.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(WELCOME_SCREEN_FXML));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Minesweeper");
+        stage.setTitle(WINDOW_TITLE);
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
