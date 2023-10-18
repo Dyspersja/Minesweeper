aa37986682f4c01fc2f106f90b9864e3e4417491
Dyspersja <dyspersjamusic@gmail.com>
2023-10-18 19:01:10 +0200

    Added bomb density information to Difficulty enum

--- Minesweeper/src/main/java/com/dyspersja/minesweeper/model/Difficulty.java
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/model/Difficulty.java
@@ -1,18 +1,24 @@
 package com.dyspersja.minesweeper.model;
 
 public enum Difficulty {
-    BEGINNER("Beginner"),
-    INTERMEDIATE("Intermediate"),
-    EXPERT("Expert");
+    BEGINNER("Beginner",8),
+    INTERMEDIATE("Intermediate",7),
+    EXPERT("Expert",6);
 
     private final String label;
+    private final int bombDensityFactor;
 
-    Difficulty(String label) {
+    Difficulty(String label, int bombDensityFactor) {
         this.label = label;
+        this.bombDensityFactor = bombDensityFactor;
     }
 
     @Override
     public String toString() {
         return label;
+    }
+
+    public int getBombDensityFactor() {
+        return bombDensityFactor;
     }
 }
