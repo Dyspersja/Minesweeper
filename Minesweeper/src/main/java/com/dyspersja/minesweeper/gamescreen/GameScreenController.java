4c3cf9d5a517fd9b9e6993586aefcedd9068b0ed
Dyspersja <dyspersjamusic@gmail.com>
2023-10-19 18:18:45 +0200

    Added code for creating board grid on game screen

--- Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
@@ -2,7 +2,9 @@ package com.dyspersja.minesweeper.gamescreen;
 
 import com.dyspersja.minesweeper.model.Difficulty;
 import javafx.fxml.FXML;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.RowConstraints;
 
 public class GameScreenController {
 
@@ -10,7 +12,31 @@ public class GameScreenController {
     private GridPane minefieldGridPane;
 
     public void initializeGameScreenController(Difficulty difficulty, int height, int width) {
+        clearGridConstraints();
 
+        addRowConstraints(height, 20);
+        addColumnConstraints(width, 20);
+    }
+
+    private void clearGridConstraints() {
+        minefieldGridPane.getRowConstraints().clear();
+        minefieldGridPane.getColumnConstraints().clear();
+    }
+
+    private void addRowConstraints(int count, double minHeight) {
+        RowConstraints row = new RowConstraints();
+        row.setMinHeight(minHeight);
+        for (int i = 0; i < count; i++) {
+            minefieldGridPane.getRowConstraints().add(row);
+        }
+    }
+
+    private void addColumnConstraints(int count, double minWidth) {
+        ColumnConstraints column = new ColumnConstraints();
+        column.setMinWidth(minWidth);
+        for (int i = 0; i < count; i++) {
+            minefieldGridPane.getColumnConstraints().add(column);
+        }
     }
 
 }
