98e76475362e5d67dbc8eee0c3bd20e9bca66b2c
Dyspersja <dyspersjamusic@gmail.com>
2023-10-19 19:01:55 +0200

    Added labels to grid

--- Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
@@ -2,6 +2,8 @@ package com.dyspersja.minesweeper.gamescreen;
 
 import com.dyspersja.minesweeper.model.Difficulty;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -16,6 +18,7 @@ public class GameScreenController {
 
         addRowConstraints(height, 20);
         addColumnConstraints(width, 20);
+        addLabelsToGrid(width, height);
     }
 
     private void clearGridConstraints() {
@@ -39,4 +42,26 @@ public class GameScreenController {
         }
     }
 
+    private void addLabelsToGrid(int width, int height) {
+        for (int column = 0; column < width; column++) {
+            for (int row = 0; row < height; row++) {
+                createLabel(column, row);
+            }
+        }
+    }
+
+    private void createLabel(int column, int row) {
+        Label label = new Label();
+
+        label.setMaxWidth(Double.MAX_VALUE);
+        label.setAlignment(Pos.CENTER);
+
+        label.setOnMouseClicked(mouseEvent ->
+                startGame(column, row));
+
+        minefieldGridPane.add(label, column, row);
+    }
+
+    private void startGame(int firstMoveColumn, int firstMoveRow) {
+    }
 }
