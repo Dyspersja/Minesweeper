f260f3182b1b0c02b21990f896fa2e8d1520c75d
Dyspersja <dyspersjamusic@gmail.com>
2023-10-19 18:14:19 +0200

    Added game screen controller and fxml file

---
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
@@ -0,0 +1,16 @@
+package com.dyspersja.minesweeper.gamescreen;
+
+import com.dyspersja.minesweeper.model.Difficulty;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class GameScreenController {
+
+    @FXML // fx:id="minefieldGridPane"
+    private GridPane minefieldGridPane;
+
+    public void initializeGameScreenController(Difficulty difficulty, int height, int width) {
+
+    }
+
+}
