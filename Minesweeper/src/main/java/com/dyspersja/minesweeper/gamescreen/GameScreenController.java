5374f4d24d52814561b77288f8b1c9ace1080774
Dyspersja <dyspersjamusic@gmail.com>
2023-11-01 10:17:09 +0100

    Added new class Tile and used it instead of class Label

--- Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/gamescreen/GameScreenController.java
@@ -1,10 +1,10 @@
 package com.dyspersja.minesweeper.gamescreen;
 
 import com.dyspersja.minesweeper.model.Difficulty;
+import com.dyspersja.minesweeper.model.Tile;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Label;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.RowConstraints;
@@ -29,7 +29,7 @@ public class GameScreenController {
         addRowConstraints(height, 20);
         addColumnConstraints(width, 20);
 
-        addLabelsToGrid(width, height);
+        addTilesToGrid(width, height);
         applyGridPaneStyles();
     }
 
@@ -58,28 +58,28 @@ public class GameScreenController {
         }
     }
 
-    private void addLabelsToGrid(int width, int height) {
+    private void addTilesToGrid(int width, int height) {
         for (int column = 0; column < width; column++) {
             for (int row = 0; row < height; row++) {
-                createLabel(column, row);
+                createTile(column, row);
             }
         }
     }
 
-    private void createLabel(int column, int row) {
-        Label label = new Label();
+    private void createTile(int column, int row) {
+        Tile tile = new Tile();
 
-        label.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
-        label.setAlignment(Pos.CENTER);
+        tile.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+        tile.setAlignment(Pos.CENTER);
 
-        label.setOnMouseClicked(mouseEvent -> {
+        tile.setOnMouseClicked(mouseEvent -> {
                     startGame(column, row);
-                    onTileClicked(label);
+                    onTileClicked(tile);
                 });
 
-        applyLabelStyle(label, "#aaaaaa");
+        applyTileStyle(tile, "#aaaaaa");
         
-        minefieldGridPane.add(label, column, row);
+        minefieldGridPane.add(tile, column, row);
     }
 
     private void applyGridPaneStyles() {
@@ -88,8 +88,8 @@ public class GameScreenController {
         );
     }
 
-    private void applyLabelStyle(Label label, String color) {
-        label.setStyle(
+    private void applyTileStyle(Tile tile, String color) {
+        tile.setStyle(
                 "-fx-font-weight: bold;" +
                 "-fx-background-color: " + color + ";" +
                 "-fx-border-color: #666666;" +
@@ -124,44 +124,44 @@ public class GameScreenController {
 
     private void updateTilesOnMouseClickAction() {
         for (Node node : minefieldGridPane.getChildren()) {
-            if (node instanceof Label label) {
-                label.setOnMouseClicked(mouseEvent -> {
-                    label.setOnMouseClicked(null);
-                    onTileClicked(label);
+            if (node instanceof Tile tile) {
+                tile.setOnMouseClicked(mouseEvent -> {
+                    tile.setOnMouseClicked(null);
+                    onTileClicked(tile);
                 });
             }
         }
     }
 
-    private void removeTilesOnMouseClickAction(Label label) {
-        label.setOnMouseClicked(null);
+    private void removeTilesOnMouseClickAction(Tile tile) {
+        tile.setOnMouseClicked(null);
     }
 
-    private void onTileClicked(Label label) {
-        int row = GridPane.getRowIndex(label);
-        int column = GridPane.getColumnIndex(label);
+    private void onTileClicked(Tile tile) {
+        int row = GridPane.getRowIndex(tile);
+        int column = GridPane.getColumnIndex(tile);
 
         if(!bombLocations[row][column]) {
             revealedTiles[row][column] = true;
 
             int adjacentBombCount = getAdjacentBombCount(row,column);
             if (adjacentBombCount != 0)
-                revealTile(label, Integer.toString(adjacentBombCount), "#eeeeee");
-            else revealAdjacentTiles(label, row, column);
-        } else revealAllBombTiles(label);
+                revealTile(tile, Integer.toString(adjacentBombCount), "#eeeeee");
+            else revealAdjacentTiles(tile, row, column);
+        } else revealAllBombTiles(tile);
     }
 
-    private void revealTile(Label label, String symbol, String color) {
-        label.setText(symbol);
-        applyLabelStyle(label, color);
-        removeTilesOnMouseClickAction(label);
+    private void revealTile(Tile tile, String symbol, String color) {
+        tile.setText(symbol);
+        applyTileStyle(tile, color);
+        removeTilesOnMouseClickAction(tile);
     }
 
-    private void revealAdjacentTiles(Label label, int row, int column) {
-        revealTile(label, "", "#eeeeee");
+    private void revealAdjacentTiles(Tile tile, int row, int column) {
+        revealTile(tile, "", "#eeeeee");
 
         for (Node node : minefieldGridPane.getChildren()) {
-            if (node instanceof Label tileLabel) {
+            if (node instanceof Tile currentTile) {
                 int r = GridPane.getRowIndex(node);
                 int c = GridPane.getColumnIndex(node);
                 if (r >= row - 1 && r <= row + 1) {
@@ -171,8 +171,8 @@ public class GameScreenController {
 
                             int adjacentBombCount = getAdjacentBombCount(r,c);
                             if (adjacentBombCount != 0)
-                                revealTile(tileLabel, Integer.toString(adjacentBombCount), "#eeeeee");
-                            else revealAdjacentTiles(tileLabel, r, c);
+                                revealTile(currentTile, Integer.toString(adjacentBombCount), "#eeeeee");
+                            else revealAdjacentTiles(currentTile, r, c);
                         }
                     }
                 }
@@ -180,15 +180,15 @@ public class GameScreenController {
         }
     }
 
-    private void revealAllBombTiles(Label label) {
+    private void revealAllBombTiles(Tile tile) {
         for (Node node : minefieldGridPane.getChildren()) {
-            if (node instanceof Label tileLabel) {
+            if (node instanceof Tile currentTile) {
                 int r = GridPane.getRowIndex(node);
                 int c = GridPane.getColumnIndex(node);
-                if (bombLocations[r][c]) revealTile(tileLabel, "X", "yellow");
+                if (bombLocations[r][c]) revealTile(currentTile, "X", "yellow");
             }
         }
-        revealTile(label, "X", "red");
+        revealTile(tile, "X", "red");
     }
 
     private int getAdjacentBombCount(int row, int column) {
---
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/model/Tile.java
@@ -0,0 +1,6 @@
+package com.dyspersja.minesweeper.model;
+
+import javafx.scene.control.Label;
+
+public class Tile extends Label {
+}
