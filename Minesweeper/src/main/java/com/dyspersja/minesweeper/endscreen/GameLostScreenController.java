b7b3c96f5f09255d82555c3866bed173b160cf86
Dyspersja <dyspersjamusic@gmail.com>
2023-11-02 09:44:14 +0100

    Added end screen controller classes

---
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/endscreen/GameLostScreenController.java
@@ -0,0 +1,13 @@
+package com.dyspersja.minesweeper.endscreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GameLostScreenController {
+
+    @FXML // fx:id="mainMenuButton"
+    private Button mainMenuButton;
+
+    @FXML // fx:id="tryAgainButton"
+    private Button tryAgainButton;
+}
---
+++ Minesweeper/src/main/java/com/dyspersja/minesweeper/endscreen/GameWonScreenController.java
@@ -0,0 +1,10 @@
+package com.dyspersja.minesweeper.endscreen;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class GameWonScreenController {
+
+    @FXML // fx:id="mainMenuButton"
+    private Button mainMenuButton;
+}
